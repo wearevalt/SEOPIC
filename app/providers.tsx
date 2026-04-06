cbc30776a5b562d7ca94e13e111a64c1a2be9773
VALT <wearevalt@gmail.com>
2026-04-06 23:40:06 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,10 @@
 
 import { SessionProvider } from 'next-auth/react'
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+type ProvidersProps = {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
   return <SessionProvider>{children}</SessionProvider>
 }
